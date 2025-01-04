101423a459314d2ec0af3aaba376b4d1fd9eb669
JeongIna <jeongina89@gmail.com>
2025-01-04 15:36:35 +0900

    [refactor] JwtService 코드 리팩토링하여 코드 가독성 향상하고 트랜잭션 적용

--- backend/src/main/java/com/ttogal/common/util/JwtService.java
+++ backend/src/main/java/com/ttogal/common/util/JwtService.java
@@ -18,9 +18,11 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import javax.crypto.SecretKey;
 import javax.crypto.spec.SecretKeySpec;
+import javax.swing.text.DefaultEditorKit;
 import java.nio.charset.StandardCharsets;
 import java.util.Date;
 import java.util.Optional;
@@ -50,83 +52,25 @@ public class JwtService {
 
   private final RefreshTokenRepository refreshTokenRepository;
 
-  private SecretKey getSecretKey(){
-    return new SecretKeySpec(secretKey.getBytes(StandardCharsets.UTF_8), Jwts.SIG.HS256.key().build().getAlgorithm());
-  }
-
-  public Optional<String> getUsername(String token){
-    return Optional.ofNullable(getClaimsJws(token).getPayload().get("username", String.class));
-  }
-
-  public String getRole(String token){
-    return getClaimsJws(token).getPayload().get("role", String.class);
-  }
-
-  public boolean isTokenValid(String token) {
-    getClaimsJws(token);
-    return true;
-  }
-
-  private Jws<Claims> getClaimsJws(String token) {
-    return Jwts.parser()
-            .verifyWith(getSecretKey())
-            .build()
-            .parseSignedClaims(token);
-  }
-
   public String createAccessToken(String username, String role) {
-  return Jwts.builder()
-          .claim("category","AccessToken")
-          .claim("username",username)
-          .claim("role",role)
-          .issuedAt(new Date(System.currentTimeMillis()))
-          .expiration(new Date(System.currentTimeMillis()+accessTokenExpirationPeriod))
-          .signWith(getSecretKey())
-          .compact();
-  }
-
-  public String createRefreshToken(String username, String role) {
-    String token= Jwts.builder()
-            .claim("category","RefreshToken")
+    return Jwts.builder()
+            .claim("category","AccessToken")
             .claim("username",username)
             .claim("role",role)
             .issuedAt(new Date(System.currentTimeMillis()))
-            .expiration(new Date(System.currentTimeMillis()+refreshTokenExpirationPeriod))
+            .expiration(new Date(System.currentTimeMillis()+accessTokenExpirationPeriod))
+            .signWith(getSecretKey())
             .compact();
-
-    RefreshToken refreshToken=RefreshToken.builder().refreshToken(token)
-            .authKey(username)
-            .build();
-
-    refreshTokenRepository.save(refreshToken);
-    return token;
   }
 
-  public Optional<String> extractAccessToken(HttpServletRequest request) {
-    return Optional.ofNullable(request.getHeader(accessHeader))
-            .filter(accessToken -> accessToken.startsWith(BEARER))
-            .map(accessToken -> accessToken.replace(BEARER, ""));
+  @Transactional
+  public String createRefreshToken(String username, String role) {
+   String refreshToken=generateRefreshToken(username, role);
+    saveRefreshToken(username, refreshToken);
+    return refreshToken;
   }
 
-  public void sendAccessAndRefreshToken(HttpServletResponse response, String accessToken, String refreshToken) {
-    response.setStatus(HttpServletResponse.SC_OK);
-    setAccessTokenHeader(response,accessToken);
-    setRefreshTokenCookie(response,refreshToken);
-  }
-
-  private void setAccessTokenHeader(HttpServletResponse response, String accessToken) {
-  response.addHeader(accessHeader, BEARER + accessToken);
-  }
-
-  private void setRefreshTokenCookie(HttpServletResponse response, String refreshToken) {
-    Cookie cookie = new Cookie(refreshHeader, refreshToken);
-    cookie.setHttpOnly(true);
-    cookie.setSecure(true);
-    cookie.setPath("/");
-    cookie.setMaxAge((int) (refreshTokenExpirationPeriod / 1000));
-    response.addCookie(cookie);
-  }
-
+  @Transactional(readOnly = true)
   public void reIssueAccessToken(HttpServletResponse response,String refreshToken) {
     log.info("Access Token 재발급 시도: {}", refreshToken);
     refreshTokenRepository.findByRefreshToken(refreshToken)
@@ -142,5 +86,75 @@ public class JwtService {
               log.warn("Access Token 재발급 실패");
               throw new RefreshTokenException(ExceptionCode.NOT_FOUND_REFRESH_TOKEN);
             });
+  }
+
+  public Optional<String> getUsername(String token){
+    return Optional.ofNullable(getClaimsJws(token).getPayload().get("username", String.class));
+  }
+
+  public String getRole(String token){
+    return getClaimsJws(token).getPayload().get("role", String.class);
+  }
+
+  public boolean isTokenValid(String token) {
+    getClaimsJws(token);
+    return true;
+  }
+
+
+  public Optional<String> extractAccessToken(HttpServletRequest request) {
+    return Optional.ofNullable(request.getHeader(accessHeader))
+            .filter(accessToken -> accessToken.startsWith(BEARER))
+            .map(accessToken -> accessToken.replace(BEARER, ""));
+  }
+
+  public void sendAccessAndRefreshToken(HttpServletResponse response, String accessToken, String refreshToken) {
+    response.setStatus(HttpServletResponse.SC_OK);
+    setAccessTokenHeader(response,accessToken);
+    setRefreshTokenCookie(response,refreshToken);
+  }
+
+  private void saveRefreshToken(String username, String token) {
+    RefreshToken refreshToken = RefreshToken.builder()
+            .refreshToken(token)
+            .authKey(username)
+            .build();
+    refreshTokenRepository.save(refreshToken);
+  }
+
+  private String generateRefreshToken(String username, String role) {
+    return Jwts.builder()
+            .claim("category", "RefreshToken")
+            .claim("username", username)
+            .claim("role", role)
+            .issuedAt(new Date(System.currentTimeMillis()))
+            .expiration(new Date(System.currentTimeMillis() + refreshTokenExpirationPeriod))
+            .compact();
+  }
+
+  private SecretKey getSecretKey(){
+    return new SecretKeySpec(secretKey.getBytes(StandardCharsets.UTF_8), Jwts.SIG.HS256.key().build().getAlgorithm());
+  }
+
+
+  private Jws<Claims> getClaimsJws(String token) {
+    return Jwts.parser()
+            .verifyWith(getSecretKey())
+            .build()
+            .parseSignedClaims(token);
+  }
+
+
+  private void setAccessTokenHeader(HttpServletResponse response, String accessToken) {
+    response.addHeader(accessHeader, BEARER + accessToken);
+  }
+
+  private void setRefreshTokenCookie(HttpServletResponse response, String refreshToken) {
+    Cookie cookie = new Cookie(refreshHeader, refreshToken);
+    cookie.setHttpOnly(true);
+    cookie.setSecure(true);
+    cookie.setPath("/");
+    cookie.setMaxAge((int) (refreshTokenExpirationPeriod / 1000));
+    response.addCookie(cookie);
   }
 }
